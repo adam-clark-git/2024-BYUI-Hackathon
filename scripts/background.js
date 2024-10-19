d760ae26f908872a7694e8bc8efefd60f46e773e
Ryan Lyman <ryanlyman@gmail.com>
2024-10-19 10:03:09 -0600

    alarm stuff

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,30 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed");
+});
+
+
+// Listen for alarms
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'workSession') {
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icon.png',  // Add your extension's icon here
+            title: 'Work Session Completed',
+            message: 'Good job! Your work session is over!',
+            priority: 2
+        });
+    } else if (alarm.name.startsWith('break_')) {
+        const breakNumber = alarm.name.split('_')[1];
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: `Break ${breakNumber}`,
+            message: `It's time for a break! Take a rest.`,
+            priority: 2
+        });
+    }
+});
+
 
 chrome.runtime.onMessage.addListener((request) => {
     if (request.type === 'startWorkSession') {
@@ -7,13 +34,28 @@ chrome.runtime.onMessage.addListener((request) => {
 
 
 
-    function startWorkSession(workTime, numberOfBreaks, breakLength) {
+        function startWorkSession(workTime, numberOfBreaks, breakLength) {
 
-        alert('Work session started! Stay focused.');
-        //chrome.action.setPopup({ popup: 'running.html' });
-        //window.close();
-        // Send workTime, numberOfBreaks, and breakLength to serviceWorker.
-        
+            alert('Work session started! Stay focused.');
 
-    },
+            chrome.alarms.clearAll();
+
+
+            chrome.alarms.create('workSession', { delayInMinutes: workTime });
+
+
+
+            // const breakInterval = workTime / (numberOfBreaks + 1);
+
+            // for (let i = 1; i <= numberOfBreaks; i++) {
+            //     chrome.alarms.create(`break_${i}`, { delayInMinutes: breakInterval * i });
+            // }
+
+
+            //chrome.action.setPopup({ popup: 'running.html' });
+            //window.close();
+            // Send workTime, numberOfBreaks, and breakLength to serviceWorker.
+
+        };
+    }
 });
--- scripts/start.js
+++ scripts/start.js
@@ -9,93 +9,91 @@ document.getElementById('start-btn').addEventListener('click', function () {
         alert('Please enter valid values for all fields.');
         return;
     }
-    
+
 
     chrome.runtime.sendMessage({
         type: 'startWorkSession',
         workTime: workTime,
         numberOfBreaks: numberOfBreaks,
         breakLength: breakLength
-    }, (response) => {
-        console.log(response.status);
     });
 
     window.close();
 
 });
 
-    // // TEMP FOR TESTING get the real list from options
-    // const urlsList = [
-    //     "https://www.coolmathgames.com/",
-    //     "https://www.other.com",
-    //     "https://www.youtube.com/*"
-    // ];
-    /*
+// // TEMP FOR TESTING get the real list from options
+// const urlsList = [
+//     "https://www.coolmathgames.com/",
+//     "https://www.other.com",
+//     "https://www.youtube.com/*"
+// ];
+/*
 
-    let currentWorkTime;
-    let currentBreaksLeft = numberOfBreaks;
-    let breakTimer;
+let currentWorkTime;
+let currentBreaksLeft = numberOfBreaks;
+let breakTimer;
 
-    
+ 
 
 
-    // Should run whenever a user goes to a flagged website
-    function OnBreakCheck() {
-        var OnBreak = prompt("Are you on break? (Yes/No)", "No")
-        if (OnBreak == "No" || OnBreak == "no")
-            alert("Best get back to work")
-        else {
-            alert("ahhh");
-            IsOnBreak();
-        }
+// Should run whenever a user goes to a flagged website
+function OnBreakCheck() {
+    var OnBreak = prompt("Are you on break? (Yes/No)", "No")
+    if (OnBreak == "No" || OnBreak == "no")
+        alert("Best get back to work")
+    else {
+        alert("ahhh");
+        IsOnBreak();
     }
+}
 
 
 
-    // Should run whenever user selects to go on a break
-    function IsOnBreak() {
-        //checks if there are no more breaks
-        if (currentWorkTime != null) {
-            currentWorkTime.pause();
-        }
-        if (checkNumBreaksIsZero) {
-            return
-        }
-        // checks if there was a timer already running
-        if (breakTimer != null) {
-            breakTimer.resume();
-            return
-        }
-        // starts a new timer
-        var breakTimer = new Timer(function () {
-            alert("Break Over");
-            currentBreaksLeft += -1;
-            breakTimer = null;
-        }, 1000 * (breakLength / 60))
+// Should run whenever user selects to go on a break
+function IsOnBreak() {
+    //checks if there are no more breaks
+    if (currentWorkTime != null) {
+        currentWorkTime.pause();
     }
-
-    // starts the work time and pauses? the break timer i think that needs to change
-    function returnToWork() {
-        if (breakTimer != null) {
-            breakTimer.pause();
-        }
-        currentWorkTime.resume();
+    if (checkNumBreaksIsZero) {
+        return
     }
-
-    // check if break count is zero
-    function checkNumBreaksIsZero() {
-        if (currentBreaksLeft <= 0 && numberOfBreaks != 1) {
-            alert("NO MORE BREAKS :(");
-            return true;
-        }
-        return false
+    // checks if there was a timer already running
+    if (breakTimer != null) {
+        breakTimer.resume();
+        return
     }
-    function ShutOff() {
+    // starts a new timer
+    var breakTimer = new Timer(function () {
+        alert("Break Over");
+        currentBreaksLeft += -1;
+        breakTimer = null;
+    }, 1000 * (breakLength / 60))
+}
 
+// starts the work time and pauses? the break timer i think that needs to change
+function returnToWork() {
+    if (breakTimer != null) {
+        breakTimer.pause();
     }
+    currentWorkTime.resume();
+}
 
-    startWorkSession()
-    */
+// check if break count is zero
+function checkNumBreaksIsZero() {
+    if (currentBreaksLeft <= 0 && numberOfBreaks != 1) {
+        alert("NO MORE BREAKS :(");
+        return true;
+    }
+    return false
+}
+function ShutOff() {
+
+}
+
+startWorkSession()
+*/
 
 
 
@@ -117,24 +115,24 @@ function urlChecker(currentUrl) {
         OnBreakCheck()
     }
 }
- /*
+/*
 var Timer = function (callback, delay) {
-    var timerId, start, remaining = delay;
+   var timerId, start, remaining = delay;
 
-    this.pause = function () {
-        window.clearTimeout(timerId);
-        timerId = null;
-        remaining -= Date.now() - start;
-    };
+   this.pause = function () {
+       window.clearTimeout(timerId);
+       timerId = null;
+       remaining -= Date.now() - start;
+   };
 
-    this.resume = function () {
-        if (timerId) {
-            return;
-        }
+   this.resume = function () {
+       if (timerId) {
+           return;
+       }
 
-        start = Date.now();
-        timerId = window.setTimeout(callback, remaining);
-    };
+       start = Date.now();
+       timerId = window.setTimeout(callback, remaining);
+   };
 
-    this.resume();
+   this.resume();
 };*/
