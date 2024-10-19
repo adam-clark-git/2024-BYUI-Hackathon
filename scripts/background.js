60ca5c82abeb5a614b411818f5908f75f7b5979d
Ryan Lyman <ryanlyman@gmail.com>
2024-10-18 22:57:20 -0600

    background url checker

---
+++ scripts/background.js
@@ -0,0 +1,24 @@
+// TEMP FOR TESTING get the real list for options
+const urlsList = [
+    "https://www.coolmath.com",
+    "https://www.other.com"
+];
+
+chrome.tabs.onUpdated.addListener((changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url) {
+        urlChecker(tab.url);
+    }
+});
+
+
+function urlChecker(currentUrl) {
+    const match = restrictedUrls.some(urlsList => currentUrl.includes(urlsList));
+
+    if (match) {
+        console.log(`Access to ${currentUrl} is restricted.`);
+        // You can implement further actions here, such as blocking the tab or showing a notification
+        chrome.tabs.update(tabId, { url: "https://www.yourblockpage.com" }); // Redirect to a block page
+    } else {
+        console.log(`Access to ${currentUrl} is allowed.`);
+    }
+}
