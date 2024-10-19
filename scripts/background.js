84a20467798216b74409b91f936116b716b8ae44
Ryan Lyman <ryanlyman@gmail.com>
2024-10-19 01:19:44 -0600

    moving stuff to background

--- scripts/background.js
+++ scripts/background.js
@@ -20,8 +20,58 @@ function urlChecker(currentUrl) {
     const match = lowerUrlsList.some(urlsList => lowerCurrentUrl.includes(urlsList));
 
     if (match) {
-        chrome.runtime.sendMessage({ action: "OnBreakCheck"})
-        // OnBreakCheck()
-    } 
+
+        OnBreakCheck()
+    }
 }
 
+
+// Should run whenever a user goes to a flagged website
+function OnBreakCheck() {
+    var OnBreak = prompt("Are you on break? (Yes/No)", "No")
+    if (OnBreak == "No" || OnBreak == "no")
+        alert("Best get back to work")
+    else {
+        IsOnBreak()
+    }
+}
+
+
+
+// Should run whenever user selects to go on a break
+function IsOnBreak() {
+    //checks if there are no more breaks
+    if (currentWorkTime != null) {
+        currentWorkTime.pause();
+    }
+    if (checkNumBreaksIsZero) {
+        return
+    }
+    // checks if there was a timer already running
+    if (breakTimer != null) {
+        breakTimer.resume();
+        return
+    }
+    // starts a new timer
+    var breakTimer = new Timer(function () {
+        alert("Break Over");
+        currentBreaksLeft += -1;
+        breakTimer = null;
+    }, 1000 * (breakLength / 60))
+}
+
+// starts the work time and pauses? the break timer i think that needs to change
+function returnToWork() {
+    if (breakTimer != null) {
+        breakTimer.pause();
+    }
+    currentWorkTime.resume();
+}
+
+// check if break count is zero
+function checkNumBreaksIsZero() {
+    if (currentBreaksLeft <= 0 && numberOfBreaks != 1) {
+        alert("NO MORE BREAKS :(");
+        return true;
+    }
+}
