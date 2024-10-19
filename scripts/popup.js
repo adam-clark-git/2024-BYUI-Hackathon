501fa3827525a0d93f629bc954172250491c2406
adam-clark-git <adam.b.clark@icloud.com>
2024-10-18 22:38:10 -0600

    Added Timer Functionality

--- scripts/popup.js
+++ scripts/popup.js
@@ -23,9 +23,12 @@ document.getElementById('start-btn').addEventListener('click', function () {
         var OnBreak = prompt("Are you on break? (Yes/No)", "No")
         if (OnBreak == "No" || OnBreak == "no")
             alert("Best get back to work")
-
+        else
+            setTimeout(GetBackToWork(), 1000 * breakLength)
     }
-
+    function GetBackToWork() {
+        alert("WAKEUP WAKEUP WAKEUP");
+    }
 
     // Function to start work session
     function startWorkSession() {
